f219eba2ba0c038f01874ba29174cdac3ccdc57a
andreybyrsa <byrsa2004@bk.ru>
2023-04-01 18:06:01 +0500

    Сделал компонент Header прикрепленным к верху приложения

--- src/layouts/Header/Header.styles.ts
+++ src/layouts/Header/Header.styles.ts
@@ -3,9 +3,11 @@ import { StyleSheet } from 'react-native';
 
 const HeaderStyles = StyleSheet.create({
   header: {
+    backgroundColor: '#C3CFD7',
     width: '100%',
+    paddingTop: 0,
     paddingHorizontal: 20,
-    paddingVertical: 16,
+    paddingBottom: 16,
 
     display: 'flex',
     justifyContent: 'space-between',
