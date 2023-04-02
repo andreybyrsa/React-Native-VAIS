53e096bb65f6f2ec3a87f46be7b4b153085e604d
andreybyrsa <byrsa2004@bk.ru>
2023-04-02 16:59:55 +0500

    Рефактор кода стилей компоннета Button

--- src/components/Button/Button.styles.ts
+++ src/components/Button/Button.styles.ts
@@ -1,3 +1,4 @@
+import getCurrentColor from '../../utils/getCurrentColor'
 import { StyleSheet, ViewStyle } from 'react-native'
 
 const DefaultButton: ViewStyle = {
@@ -21,46 +22,46 @@ const ButtonStyes = StyleSheet.create({
     alignItems: 'center',
   },
   'button--primary': {
-    backgroundColor: '#264A5A',
+    backgroundColor: getCurrentColor('color-button-primary_enable'),
   },
   'button--secondary': {
-    backgroundColor: '#02202B',
+    backgroundColor: getCurrentColor('color-button-secondary_enable'),
     borderStyle: 'solid',
-    borderColor: '#00E4FF',
+    borderColor: getCurrentColor('color-border-secondary_enable'),
     borderWidth: 2,
   },
   'button-primary--disabled': {
-    backgroundColor: '#7496A6',
+    backgroundColor: getCurrentColor('color-button-primary_disable'),
   },
   'button-secondary--disabled': {
-    backgroundColor: '#7496A6',
+    backgroundColor: getCurrentColor('color-button-secondary_disable'),
     borderStyle: 'solid',
-    borderColor: '#00E4FF',
+    borderColor: getCurrentColor('color-border-secondary_enable'),
     borderWidth: 2,
   },
 
   'button-primary--active': {
-    backgroundColor: '#02202B',
+    backgroundColor: getCurrentColor('color-button-primary_active'),
   },
   'button-secondary--active': {
-    backgroundColor: '#FFFFFF',
+    backgroundColor: getCurrentColor('color-button-secondary_active'),
     borderStyle: 'solid',
-    borderColor: '#02202B',
+    borderColor: getCurrentColor('color-border-secondary_active'),
     borderWidth: 2,
   },
 
   'button__text-primary': {
-    color: '#FFFFFF',
+    color: getCurrentColor('color-text-tertiary'),
   },
   'button__text-secondary': {
-    color: '#00E4FF',
+    color: getCurrentColor('color-text-secondary'),
   },
 
   'button__text-primary--active': {
-    color: '#FFFFFF',
+    color: getCurrentColor('color-text-tertiary'),
   },
   'button__text-secondary--active': {
-    color: '#02202B',
+    color: getCurrentColor('color-text-primary'),
   },
 })
 
