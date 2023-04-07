436efb4917595b19cba997d31f99558d4be31682
andreybyrsa <byrsa2004@bk.ru>
2023-04-07 12:35:03 +0500

    Сделал компонент Image

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
+import { onBoardingContent } from './assets/images'
 import Button from './src/components/Button'
+import ImageComponent from './src/components/ImageComponent/ImageComponent'
+import React from 'react'
 import { StyleSheet, View } from 'react-native'
 
 export default function App() {
   return (
     <View style={style.container}>
-      <Button type="primary">Продолжить</Button>
+      <ImageComponent
+        style={{ height: 100, width: 100 }}
+        imageSrc={onBoardingContent}
+        alt="onboarding"
+      />
     </View>
   )
 }
