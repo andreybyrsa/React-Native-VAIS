8c868067b768f331085e780d497b8d85a576976b
andreybyrsa <byrsa2004@bk.ru>
2023-04-09 14:31:33 +0500

    Сделал компонент AppLoader

--- App.tsx
+++ App.tsx
@@ -1,9 +1,35 @@
-import AppLoader from './src/components/Loaders/AppLoader/AppLoader'
+import { logo } from './assets/images'
+import Cell from './src/components/Cell'
+import ImageComponent from './src/components/ImageComponent'
 import React from 'react'
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
-  return <AppLoader />
+  return (
+    <View style={style.container}>
+      <Cell
+        before={
+          <View>
+            <Text>ИКОНКА</Text>
+          </View>
+        }
+        after={
+          <View>
+            <Text>ИКОНКА</Text>
+          </View>
+        }
+        subtitle="Избранное"
+        text="Какой-то текст, который поразит мир"
+      />
+      <ImageComponent
+        style={{ height: 200, width: 200 }}
+        wrapperStyle={{ height: 200, width: 200 }}
+        imageSrc={logo}
+        alt="image"
+        isWrapped
+      />
+    </View>
+  )
 }
 
 const style = StyleSheet.create({
