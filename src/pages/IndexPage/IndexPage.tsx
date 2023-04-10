16c040c1bd2952445863f5bfb06910240a98ab2d
andreybyrsa <byrsa2004@bk.ru>
2023-04-10 19:08:15 +0500

    Сделал базово страницу IndexPage

---
+++ src/pages/IndexPage/IndexPage.tsx
@@ -0,0 +1,40 @@
+import IconComponent from '../../components/IconComponent'
+import NavigationSideBar from '../../components/NavigationComponents/NavigationSideBar'
+import Typography from '../../components/Typography'
+import Footer from '../../layouts/Footer'
+import Header from '../../layouts/Header'
+import PageLayout from '../../layouts/PageLayout'
+import React, { useMemo } from 'react'
+
+function IndexPage() {
+  const header = useMemo(() => {
+    return (
+      <Header
+        leftSideSlot={<Typography variant="title-1">Главная</Typography>}
+        rightSideSlot={
+          <IconComponent
+            iconName="ios-heart-outline"
+            size={45}
+          />
+        }
+      />
+    )
+  }, [])
+
+  const footer = useMemo(() => {
+    return (
+      <Footer isNavigation>
+        <NavigationSideBar />
+      </Footer>
+    )
+  }, [])
+
+  return (
+    <PageLayout
+      header={header}
+      footer={footer}
+    />
+  )
+}
+
+export default IndexPage
