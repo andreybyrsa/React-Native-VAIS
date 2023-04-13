5fc7e70b220b54775a2e5330ba78322f459694b8
andreybyrsa <byrsa2004@bk.ru>
2023-04-13 18:16:28 +0500

    Сделал страницу профиля

---
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -0,0 +1,76 @@
+import Button from '../../components/Button'
+import Cell from '../../components/Cell'
+import IconComponent from '../../components/IconComponent'
+import NavigationSideBar from '../../components/NavigationComponents/NavigationSideBar'
+import Typography from '../../components/Typography'
+import Footer from '../../layouts/Footer'
+import Header from '../../layouts/Header'
+import PageLayout from '../../layouts/PageLayout'
+import CellContentType from '../../types/CellContentType'
+import ProfilePageStyle from './ProfilePage.styles'
+import { useMemo } from 'react'
+import { View } from 'react-native'
+
+const Cells: CellContentType[] = [
+  {
+    id: 0,
+    title: 'Избранное',
+  },
+  {
+    id: 1,
+    title: 'Оценить приложение',
+  },
+  {
+    id: 2,
+    title: 'Связаться с нами',
+  },
+]
+
+function ProfilePage() {
+  const header = useMemo(() => {
+    return <Header leftSideSlot={<Typography variant="title-1">Профиль</Typography>} />
+  }, [])
+
+  const footer = useMemo(() => {
+    return (
+      <Footer isNavigation>
+        <NavigationSideBar />
+      </Footer>
+    )
+  }, [])
+
+  return (
+    <PageLayout
+      contentStyle={ProfilePageStyle['profile-page__content']}
+      header={header}
+      footer={footer}
+    >
+      <View style={ProfilePageStyle['profile-page__profile-info']}>
+        <IconComponent
+          iconName="ios-person-circle"
+          size={70}
+        />
+        <View>
+          <Typography variant="title-3">Имя: Kirill</Typography>
+          <Typography variant="title-3">Телефон: 81112224455</Typography>
+        </View>
+      </View>
+
+      {Cells.map((elem: CellContentType) => (
+        <Cell
+          key={elem.id}
+          title={elem.title}
+          after={
+            <IconComponent
+              iconName="md-chevron-forward"
+              size={35}
+            />
+          }
+        />
+      ))}
+      <Button type="primary">Перейти на премиум</Button>
+    </PageLayout>
+  )
+}
+
+export default ProfilePage
---
+++ src/types/CellContentType.ts
@@ -0,0 +1,6 @@
+interface CellContentType {
+  id: number
+  title: string
+}
+
+export default CellContentType
