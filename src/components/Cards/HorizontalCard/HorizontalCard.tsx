b03dae0ad81134097c50ebddf91a3e90e7a363fc
andreybyrsa <byrsa2004@bk.ru>
2023-05-16 21:33:40 +0500

    Merge branch task#modal

--- src/components/Cards/HorizontalCard/HorizontalCard.tsx
+++ src/components/Cards/HorizontalCard/HorizontalCard.tsx
@@ -39,7 +39,7 @@ function HorizontalCard({
       <View style={[HorizontalCardStyle[`HorizontalCard__clothes-content--${type}`]]}>
         <View style={[HorizontalCardStyle['HorizontalCard__image']]}>
           <ImageComponent
-            style={{width: 100, height: 100}}
+            style={{ width: 100, height: 100 }}
             imageSrc={imageSrc}
             alt="clothes"
           />
